2b00eeb0ce6f6af3d7ab27c6014c4261a068f454
Himanshu Malik <himanshumalik552@gmail.com>
2024-06-11 13:49:24 +0530

    change env uri part

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
     reactStrictMode: true,
     serverRuntimeConfig: {
-        connectionString: "mongodb://localhost/next-js-registration-login-example",
-        secret: process.env.SECRET_KEY
+        connectionString: process.env.SECRET_KEY,
+        secret: process.env.MONGODB_URI
     },
     publicRuntimeConfig: {
         apiUrl: process.env.API_URI
